054e8d74beb57c4fbcbef81d7cf4a038775218e7
Carl Stjernfeldt <carl.elgestad@gmail.com>
2018-02-04 13:02:21 +0100

    Columns are dynamically created based on content in table 'Status'

--- ProjectOverviewManager/ProjectOverviewManager/App.xaml.cs
+++ ProjectOverviewManager/ProjectOverviewManager/App.xaml.cs
@@ -14,4 +14,5 @@ namespace ProjectOverviewManager
     public partial class App : Application
     {
     }
+
 }
--- ProjectOverviewManager/ProjectOverviewManager/MainWindow.xaml.cs
+++ ProjectOverviewManager/ProjectOverviewManager/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SQLite;
 
 namespace ProjectOverviewManager
 {
@@ -20,9 +21,29 @@ namespace ProjectOverviewManager
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SQLiteConnection connection;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            connection = new SQLiteConnection(@"Data source=C:\Users\Administrator\Documents\GitHub\ProjectOverviewManager\db\main_db_file.db");
+            connection.Open();
+            SQLiteCommand cmd = connection.CreateCommand();
+            cmd.CommandText = "SELECT * FROM Status";
+            using (SQLiteDataReader rdr = cmd.ExecuteReader())
+            {
+                while (rdr.Read())
+                {
+                    // För varje status i svaret från databasen
+                    // skapar vi kolumner dynamiskt
+                    GridViewColumn column = new GridViewColumn();
+                    column.Header = rdr.GetString(1);
+                    column.Width = 200;
+                    MyGridView.Columns.Add(column);
+                }
+            }
+            connection.Close();
         }
 
         private void ProjectListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
