10191647d75491a7eaf66b1f67f620b3d55ba808
fredrik ahlberg <fredr@DESKTOP-75I7LL1>
2018-02-04 11:18:03 +0100

    aafe

--- ProjectOverviewManager/ProjectOverviewManager/MenuControl/UserControl1.xaml.cs
+++ ProjectOverviewManager/ProjectOverviewManager/MenuControl/UserControl1.xaml.cs
@@ -13,21 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ProjectOverviewManager
+namespace ProjectOverviewManager.MenuControl
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for UserControl1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class UserControl1 : UserControl
     {
-        public MainWindow()
+        public UserControl1()
         {
             InitializeComponent();
-        }
-
-        private void ProjectListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
         }
     }
 }
