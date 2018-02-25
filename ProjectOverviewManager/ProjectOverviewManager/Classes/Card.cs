de2823697f37ea3a7dc46f26c046bbc7da2b2991
Carl Stjernfeldt <carl.elgestad@gmail.com>
2018-02-25 15:23:26 +0100

    #5 Added card class

---
+++ ProjectOverviewManager/ProjectOverviewManager/Classes/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectOverviewManager
+{
+    class Card {
+        private String title;
+        private String description;
+        private String date;
+
+        public Card(String title, String description, String date)
+        {
+            this.title = title;
+            this.description = description;
+            this.date = date;
+        }
+    }
+}
